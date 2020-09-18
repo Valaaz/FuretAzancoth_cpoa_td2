6510229f41c22ba937160254c01a4b4be88e0898
azancoth1u <azancoth1u@iutm-inf-e37-02.iutmetz.site.univ-lorraine.fr>
2020-09-18 16:19:43 +0200

    Ajout de Commande et LigneCommande en POJO

---
+++ src/metier/Commande.java
@@ -0,0 +1,61 @@
+package metier;
+import java.time.*;
+
+public class Commande {
+	private int idCommande;
+	private Client idClient;
+	private LocalDate date;
+	
+	public Commande(int idCommande, Client idClient, LocalDate date) {
+		super();
+		this.idCommande = idCommande;
+		this.idClient = idClient;
+		this.date = date;
+	}
+
+	public int getIdCommande() {
+		return idCommande;
+	}
+
+	public void setIdCommande(int idCommande) {
+		this.idCommande = idCommande;
+	}
+
+	public Client getIdClient() {
+		return idClient;
+	}
+
+	public void setIdClient(Client idClient) {
+		this.idClient = idClient;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Commande [idCommande=" + idCommande + ", idClient=" + idClient + ", date=" + date + "]";
+	}
+	
+	/*
+	- conversion d'une date MySQL (java.sql.Date) vers LocalDate :
+		maDateSQL.toLocalDate()
+
+	- conversion d'une LocalDate en date MySQL
+    	java.sql.Date.valueOf(maLocalDate)
+
+    - saisie d'une date : sous forme de chaîne, puis utilisation d'un formateur de date
+    	DateTimeFormatter formatage = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    	LocalDate dateDebut = LocalDate.parse(maStringDate, formatage);
+
+    - affichage d'une date LocalDate :
+    	formatage.format(maLocalDate)
+    */
+	
+}
--- src/metier/LigneCommande.java
+++ src/metier/LigneCommande.java
@@ -3,6 +3,58 @@ package metier;
 import java.util.HashMap;
 
 public class LigneCommande {
-	HashMap<Produit, String> listeCommande = new HashMap<Produit, String>();
+	private int idCommande;
+	private Produit idProduit;
+	private double tarifUnitaire;
+	
+	HashMap<Integer, Produit> listeCommande = new HashMap<Integer, Produit>();
+
+	public LigneCommande(int idCommande, Produit idProduit, double tarifUnitaire, HashMap<Integer, Produit> listeCommande) {
+		super();
+		this.idCommande = idCommande;
+		this.idProduit = idProduit;
+		this.tarifUnitaire = tarifUnitaire;
+		this.listeCommande = listeCommande;
+	}
+
+	public int getIdCommande() {
+		return idCommande;
+	}
+
+	public void setIdCommande(int idCommande) {
+		this.idCommande = idCommande;
+	}
+
+	public Produit getIdProduit() {
+		return idProduit;
+	}
+
+	public void setIdProduit(Produit idProduit) {
+		this.idProduit = idProduit;
+	}
+
+	public double getTarifUnitaire() {
+		return tarifUnitaire;
+	}
+
+	public void setTarifUnitaire(double tarifUnitaire) {
+		this.tarifUnitaire = tarifUnitaire;
+	}
+
+	public HashMap<Integer, Produit> getListeCommande() {
+		return listeCommande;
+	}
+
+	public void setListeCommande(HashMap<Integer, Produit> listeCommande) {
+		this.listeCommande = listeCommande;
+	}
+
+	@Override
+	public String toString() {
+		return "LigneCommande [idCommande=" + idCommande + ", idProduit=" + idProduit + ", tarifUnitaire="
+				+ tarifUnitaire + ", listeCommande=" + listeCommande + "]";
+	}
+	
+	
 	
 }
